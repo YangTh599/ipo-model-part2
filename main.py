4f2d387eac0defaa18225b67d383cc83c29f3e2e
YangTh599 <YangTh599@northwested.org>
2024-09-18 11:05:49 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# Thayer Yang
+# 18 SEP 2024
+# IPO Intro
+
+#Input
+num1 = input('Enter a number: (Example 7)\n')
+num2  = input('Enter another number: (Example: 4)\n')
+
+#Process
+total = float(num1)+ float(num2)
+
+#Output
+print(f'First number entered was: {num1}')
+print(f'Second number entered was: {num2}')
+print()
+print(f'The sum of {num1} and {num2} is: {total}')
